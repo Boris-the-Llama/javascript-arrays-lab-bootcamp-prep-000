73f3d5fbee1eee02216a01857aa9757f85af41d0
Dan Brett <boristhellama99@gmail.com>
2018-01-09 23:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var array2 = kittens.slice(kittens.length - 1);
+  var array2 = kittens.slice(0, kittens.length - 1);
   return array2;
 }
